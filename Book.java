5ff7d1888a08681680f4eeed0b00ae358c2be442
gorillion <SANICSPEEDS@GMAIL.COM>
2016-09-25 21:36:04 -0400

    85
    the secret number

--- Book.java
+++ Book.java
@@ -1,26 +1,45 @@
+import java.*;
 /**
  * A class that maintains information on a book.
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @david farrell
+ * @website
  */
-class Book
+public class Book
 {
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
-    {
+    public Book(String bookAuthor, String bookTitle, int bookPages)
+    {   
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
+    public void printAuthor()
+    {
+        System.out.println("The author is : " + author);
+    }
+    
+    public void printTitle()
+    {
+        System.out.println("The title is : " + title);
+    }
+    
+    public void printPages()
+    {
+        System.out.println("The # of pages is : " + pages);
+    }
+    
     // Add the methods here ...
 }
